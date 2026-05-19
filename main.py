bc984dd88f69dab9f719d699d507089e195a0eb1
xhrusvin <rusvin@xpresshealth.com>
2026-05-19 10:20:59 +0100

    changes

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ if __name__ == "__main__":
     app = mcp.streamable_http_app()
     uvicorn.run(
         app,
-        host="0.0.0.0",
+        host="127.0.0.1",
         port=3100,
         forwarded_allow_ips="*",
         proxy_headers=True,
